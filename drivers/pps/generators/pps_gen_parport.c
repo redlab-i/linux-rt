8b3daac57997e9ad037e0f7c775ec1f53beb9fbe
Nikita Edward Baruzdin <nebaruzdin@gmail.com>
2017-02-15 17:31:09 +0300

    WIP: LinuxPPS: pps_gen_parport: Add check for bad clocksource.
    This commit is supposed to resolve the issue with hard lockups on systems using
    jiffies as their clock source. Namely, it sets limits on number of iterations
    clock source may remain unchanged (i. e. not being updated for one reason or
    another, as it is with jiffies clock source), and on unsuccessful
    getnstimeofday() polls as well. In case limit is reached, we consider clock
    source incompatible with this driver or unstable.
    
    Considering this issue to be fixed, un-BROKEN pps_gen_parport.
    
    For explanation of the problem see this thread on lkml:
    https://lkml.org/lkml/2011/2/18/310
    
    Signed-off-by: Nikita Edward Baruzdin <nebaruzdin@lvk.cs.msu.su>
    Signed-off-by: Alexander GQ Gerasiov <gq@cs.msu.su>
    Signed-off-by: Andrey Drobyshev <immortalguardian1@gmail.com>

--- drivers/pps/generators/pps_gen_parport.c
+++ drivers/pps/generators/pps_gen_parport.c
@@ -53,6 +53,16 @@ MODULE_PARM_DESC(polarity,
 	"Signal is on the low level (0 - default) or on the high level (1).");
 module_param(polarity, uint, 0);
 
+static unsigned int failure_iterations = 10;
+MODULE_PARM_DESC(failure_iterations,
+	"Number of iterations the clock source may remain unchanged.");
+module_param(failure_iterations, uint, 0);
+
+static unsigned int gettime_attempts = 100000;
+MODULE_PARM_DESC(gettime_attempts,
+	"Total number of possible unsuccessfull gettime attempts.");
+module_param(gettime_attempts, uint, 0);
+
 #define SAFETY_INTERVAL	3000	/* set the hrtimer earlier for safety (ns) */
 
 /* internal per port structure */
@@ -79,6 +89,7 @@ static enum hrtimer_restart hrtimer_event(struct hrtimer *timer)
 	struct parport *port;
 	long lim, delta;
 	unsigned long flags;
+	unsigned int i;
 
 	/* We have to disable interrupts here. The idea is to prevent
 	 * other interrupts on the same processor to introduce random
@@ -106,8 +117,20 @@ static enum hrtimer_restart hrtimer_event(struct hrtimer *timer)
 	}
 
 	/* busy loop until the time is right for an assert edge */
+	i = 0;
 	do {
 		getnstimeofday(&ts2);
+		i++;
+
+		/* Check if there are problems with clock source
+		 * and prevent hard lockups.
+		 */
+		if (unlikely(i >= failure_iterations &&
+			ts1.tv_sec  == ts2.tv_sec &&
+			ts1.tv_nsec == ts2.tv_nsec)) {
+				pr_err("pps_gen_parport: clock stay unchanged for too long.\n");
+				goto error;
+		}
 	} while (expire_time.tv_sec == ts2.tv_sec && ts2.tv_nsec < lim);
 
 	/* set the signal */
@@ -116,8 +139,19 @@ static enum hrtimer_restart hrtimer_event(struct hrtimer *timer)
 
 	/* busy loop until the time is right for a clear edge */
 	lim = NSEC_PER_SEC - dev->port_write_time;
+	i = 0;
 	do {
 		getnstimeofday(&ts2);
+		i++;
+
+		/* Check if there are problems with clock source
+		 * and prevent hard lockups.
+		 */
+		if (unlikely(i > gettime_attempts)) {
+			pr_err("pps_gen_parport: gettime attempts threshold is exceeded\n.");
+			goto error;
+		}
+
 	} while (expire_time.tv_sec == ts2.tv_sec && ts2.tv_nsec < lim);
 
 	/* unset the signal */
@@ -154,6 +188,12 @@ done:
 				2 * hrtimer_error)));
 
 	return HRTIMER_RESTART;
+
+error:
+	local_irq_restore(flags);
+	pr_err("Clocksource unstable or not compatible with pps_gen_parport.\n");
+	return HRTIMER_NORESTART;
+
 }
 
 /* calibrate port write time */
